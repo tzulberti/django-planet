6cd9a1ac9cdb5886c23f9a0f24b963fe49a557fa
Tomas Zulberti <tomas@jampp.com>
2020-05-09 12:51:50 -0300

    Bump django-taggit version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 
     install_requires=[
         "feedparser",
-        "django-tagging==0.3.6",
+        "django-tagging>=1.2.0",
         "django-pagination-py3",
         "Django>=1.6",
         "beautifulsoup4",
