700aaad308ac8d042716577ec401b5cb3f327522
Tomas Zulberti <tomas@jampp.com>
2020-05-09 12:53:50 -0300

    Set the correct version for django-tagging

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 
     install_requires=[
         "feedparser",
-        "django-tagging>=1.2.0",
+        "django-tagging==0.5.0",
         "django-pagination-py3",
         "Django>=1.6",
         "beautifulsoup4",
